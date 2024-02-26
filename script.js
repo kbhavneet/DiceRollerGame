c76857f57bcefd83dc70f30c41a191d3617054c8
kbhavneet <155472615+kbhavneet@users.noreply.github.com>
2024-02-27 04:44:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+if (window.performance.navigation.type === 1) {
+  dice();
+}
+
+function dice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomImage1 = "images/dice" + randomNumber1 + ".png";
+
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomImage1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomImage2 = "images/dice" + randomNumber2 + ".png";
+
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomImage2);
+
+  if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 Won! 🥇";
+  } else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Won! 🏆";
+  } else {
+    document.querySelector("h1").innerHTML = "It's a Draw! 📍";
+  }
+}
